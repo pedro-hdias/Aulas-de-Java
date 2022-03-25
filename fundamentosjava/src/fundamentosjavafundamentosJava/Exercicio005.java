7a1380018b0931d995b1c04bb3d821effda7d2e4
pedro-hdias <83011148+pedro-hdias@users.noreply.github.com>
2022-03-25 06:37:31 -0300

    Aulas já realizadas

---
+++ fundamentosjava/src/fundamentosjavafundamentosJava/Exercicio005.java
@@ -0,0 +1,12 @@
+package fundamentosjavafundamentosJava;
+
+public class Exercicio005 {
+	public static void main(String[] args) {
+
+				String nome = "Pedro Dias";
+				double saldo = 1659;
+				short idade = 47;
+
+	System.out.println("Meu nome é: " + nome + "Minha idade é: " + idade + "meu saldo é: " + saldo);
+		}
+}
